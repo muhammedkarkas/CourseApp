c5d9671e394cbe35d0074b9d6a013a56b06baedc
muhammed.karkas <muhammed.karkas@gmail.com>
2023-07-27 23:39:32 +0300

    .

---
+++ CourseApp/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
--- CourseApp/Models/Candidate.cs
+++ CourseApp/Models/Candidate.cs
@@ -2,5 +2,16 @@
 {
     public class Candidate
     {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
     }
 }
